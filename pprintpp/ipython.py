0f5dbfda574a78a50f9ad7071b1ef5ae7551403e
me <you@example.com>
2018-06-24 21:54:24 +0300

    Add ipython plugin

---
+++ pprintpp/ipython.py
@@ -0,0 +1,53 @@
+import IPython
+from traitlets.config import Configurable
+from traitlets import Int
+
+from . import pformat
+
+
+original_representation = IPython.lib.pretty.RepresentationPrinter
+DEFAULT_INDENTATION = 2
+
+
+def load_ipython_extension(ipython):
+    ipython.config.PPrintPP.indentation = DEFAULT_INDENTATION
+    IPython.lib.pretty.RepresentationPrinter = PPrintPPRepresentation
+    pprintpp = PPrintPP(parent=ipython, config=ipython.config)
+    ipython.configurables.append(pprintpp)
+
+
+def unload_ipython_extension(ipython):
+    IPython.lib.pretty.RepresentationPrinter = original_representation
+    try:
+        pprintpp = [
+            configurable for configurable in ipython.configurables
+            if isinstance(configurable, PPrintPP)
+        ][0]
+    except IndexError:
+        print('Could not unload {}'.format(__name__))
+    else:
+        ipython.configurables.remove(pprintpp)
+
+
+class PPrintPPRepresentation(object):
+    """
+    A pretty printer that uses pprintpp
+    """
+
+    def __init__(self, stream, *args, **kwargs):
+        self.stream = stream
+        self.config = IPython.get_ipython().config
+
+    def pretty(self, obj):
+        indentation = self.config.PPrintPP.indentation
+        self.stream.write(pformat(obj, indent=indentation))
+
+    def flush(self):
+        pass
+
+
+class PPrintPP(Configurable):
+    """
+    PPrintPP configuration
+    """
+    indentation = Int(config=True)
